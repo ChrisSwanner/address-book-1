deb8c282ed70cab50f215377c5ee10d8f22e7e60
Joel Adams <joeladamsdesign@gmail.com>
2018-02-09 08:32:46 -0800

    add startup.cs, program.cs, and .csproj files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Address_Book
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
